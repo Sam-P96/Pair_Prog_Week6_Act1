ba53bda0594fcbf0ef826983d8cefba6132bb440
Hung-497 <giahung092007@gmail.com>
2026-09-21 15:14:57 +0300

    [iter4] Expand the User Model ...

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const { loginUser, signupUser } = require("../controllers/userController");
+const { loginUser, signupUser, getMe } = require("../controllers/userController");
+const requireAuth = require("../middleware/requireAuth");
   
 // login route
 router.post("/login", loginUser);
   
 // signup route
 router.post("/signup", signupUser);
+
+router.get("/me", requireAuth, getMe);
   
 module.exports = router;
 
